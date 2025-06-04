5a64733930c9c19bfdf900fcae059110fac57e2c
Márton Csernik <csernik.marton@gmail.com>
2025-06-04 13:27:15 +0200

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Livewire\Contact;
+use App\Livewire\Faq;
 use App\Livewire\FeedDashboard;
 use App\Livewire\ManageFeeds;
 use App\Livewire\Settings\UserProfile;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/login')->name('home');
-
-Route::get('faq', App\Livewire\Faq::class)->name('faq');
-Route::get('contact', App\Livewire\Contact::class)->name('contact');
+Route::get('faq', Faq::class)->name('faq');
+Route::get('contact', Contact::class)->name('contact');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', FeedDashboard::class)
@@ -16,7 +16,6 @@ Route::middleware(['auth'])->group(function () {
         ->name('dashboard');
 
     Route::get('feeds/manage', ManageFeeds::class)->name('feeds.manage');
-
     Route::get('profile', UserProfile::class)->name('profile');
 });
 
