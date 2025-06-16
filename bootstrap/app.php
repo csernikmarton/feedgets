6b526304fccda090143aa1cfd74d73bbb19424a9
Márton Csernik <csernik.marton@gmail.com>
2025-06-16 18:20:59 +0200

    send exceptions in email

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Mail\ExceptionMail;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +17,39 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (Exception $exception) {
+            if (app()->environment('local')) {
+                return;
+            }
+
+            if ($exception instanceof Illuminate\Auth\AuthenticationException) {
+                return;
+            }
+
+            if (method_exists($exception, 'getStatusCode') && in_array($exception->getStatusCode(), [403, 404, 405])) {
+                return;
+            }
+
+            try {
+                $e = FlattenException::createFromThrowable($exception);
+                $handler = new HtmlErrorRenderer(true); // boolean, true raises debug flag...
+                $css = $handler->getStylesheet();
+                $content = $handler->getBody($e);
+                $url = Request::fullUrl();
+
+                $userinfo = [];
+                if (auth()->check()) {
+                    $userinfo['User ID'] = auth()->user()->id;
+                    $userinfo['User Email'] = auth()->user()->email;
+                }
+                $userinfo_string = '';
+                foreach ($userinfo as $k => $v) {
+                    $userinfo_string .= sprintf('%s: %s', $k, $v).'<br>';
+                }
+
+                Mail::send(new ExceptionMail($userinfo_string, $content, $url, $css));
+            } catch (Throwable $ex) {
+                dd($ex);
+            }
+        });
     })->create();
---
+++ resources/views/emails/exception.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <style>{!! $css ?? '' !!}</style>
+</head>
+<body>
+<p>{!! $userinfo !!}</p>
+<p>{!! $url !!}</p>
+{!! $content ?? '' !!}
+</body>
+</html>
