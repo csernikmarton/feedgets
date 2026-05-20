c99a7e2c96d3039663251b5423d216c681799273
Shift <shift@laravelshift.com>
2026-05-20 07:33:50 +0000

    Remove unnecessary `$model` property

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -15,13 +15,6 @@ use Illuminate\Support\Str;
 class ArticleFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Article::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
